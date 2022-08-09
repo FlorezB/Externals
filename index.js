afa59c31b8dcdd65b6b03267d8d9d2cbaae85ccb
Caroline <alyseflorez@hotmail.fr>
2022-08-09 13:44:21 +0200

    routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const gotRoutes = require("./Routes/got")
+const gotRoutes = require("./routes/got")
 
 app.use("/game-of-thrones",gotRoutes)
 
